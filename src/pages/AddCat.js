36884db720c2b53fbf52fe84360b3dce0ea6d2db
Yash <yashrajoriya321@gmail.com>
2023-02-08 00:09:49 +0530

    update

--- src/pages/AddCat.js
+++ src/pages/AddCat.js
@@ -4,41 +4,83 @@ import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import * as yup from "yup";
 import { useFormik } from "formik";
-import { createProCat } from "../features/pcategory/pCategorySlice";
-import { resetState } from "../features/color/colorSlice";
+import {
+  createProCat,
+  getAProductCat,
+  resetState,
+  updateProductCat,
+} from "../features/pcategory/pCategorySlice";
+import { useLocation, useNavigate } from "react-router-dom";
 
 let schema = yup.object().shape({
   title: yup.string().required("Category is Required"),
 });
 const AddCat = () => {
   const dispatch = useDispatch();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const getprocatId = location.pathname.split("/")[3];
   const newProductCat = useSelector((state) => state.pCategory);
-  const { isSuccess, isError, isLoading, CreatedproCategories } = newProductCat;
+  const {
+    isSuccess,
+    isError,
+    isLoading,
+    CreatedproCategories,
+    categoryName,
+    updatedproCategories,
+  } = newProductCat;
+  useEffect(() => {
+    if (getprocatId !== undefined) {
+      dispatch(getAProductCat(getprocatId));
+    } else {
+      dispatch(resetState());
+    }
+  }, [getprocatId, dispatch]);
   useEffect(() => {
     if (isSuccess && CreatedproCategories) {
       toast.success("Category Added Successfully!");
     }
+    if (updatedproCategories && isSuccess) {
+      toast.success("Category Updated Successfully");
+      navigate("/admin/category-list");
+    }
     if (isError) {
       toast.error("Something Went Wrong!");
     }
-  }, [isSuccess, isError, isLoading, CreatedproCategories]);
+  }, [
+    isSuccess,
+    isError,
+    isLoading,
+    CreatedproCategories,
+    navigate,
+    updatedproCategories,
+  ]);
 
   const formik = useFormik({
+    enableReinitialize: true,
     initialValues: {
-      title: "",
+      title: categoryName || "",
     },
     validationSchema: schema,
     onSubmit: (values) => {
-      dispatch(createProCat(values));
-      formik.resetForm();
-      setTimeout(() => {
-        dispatch(resetState());
-      }, 3000);
+      if (getprocatId !== undefined) {
+        const data = { id: getprocatId, procatData: values };
+        dispatch(updateProductCat(data));
+      } else {
+        dispatch(createProCat(values));
+        formik.resetForm();
+        setTimeout(() => {
+          dispatch(resetState());
+        }, 300);
+      }
     },
   });
   return (
     <div>
-      <h3 className="mb-4 title">Add Category</h3>
+      <h3 className="mb-4 title">
+        {getprocatId !== undefined ? "Edit " : "Added "}
+        Category
+      </h3>
       <div>
         <form action="" onSubmit={formik.handleSubmit}>
           <CustomInput
@@ -57,7 +99,7 @@ const AddCat = () => {
             className="btn btn-success border-0 rounded-3 my-5"
             type="submit"
           >
-            Add Category
+            {getprocatId !== undefined ? "Edit " : "Added "} Category
           </button>
         </form>
       </div>
--- src/pages/CategoryList.js
+++ src/pages/CategoryList.js
@@ -1,10 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Table } from "antd";
 import { useDispatch, useSelector } from "react-redux";
-import { getProductCat } from "../features/pcategory/pCategorySlice";
+import {
+  getProductCat,
+  deleteProductCat,
+  resetState,
+} from "../features/pcategory/pCategorySlice";
 import { Link } from "react-router-dom";
 import { BiEdit } from "react-icons/bi";
 import { AiOutlineDelete } from "react-icons/ai";
+import CustomModal from "../components/CustomModal";
 
 const columns = [
   {
@@ -23,8 +28,19 @@ const columns = [
 ];
 
 const Categorylist = () => {
+  const [open, setOpen] = useState(false);
+  const [prodcatId, setprodcatId] = useState("");
+  const showModal = (e) => {
+    setOpen(true);
+    setprodcatId(e);
+  };
+  const hideModal = () => {
+    setOpen(false);
+  };
+  
   const dispatch = useDispatch();
   useEffect(() => {
+    dispatch(resetState());
     dispatch(getProductCat());
   }, [dispatch]);
   const proCatState = useSelector((state) => state.pCategory.proCategories);
@@ -35,22 +51,39 @@ const Categorylist = () => {
       name: proCatState[i].title,
       action: (
         <>
-          <Link to="/" className="fs-4">
+          <Link to={`/admin/category/${proCatState[i]._id}`} className="fs-4">
             <BiEdit />
           </Link>
-          <Link className="ms-3 text-danger fs-4" to="/">
+          <button
+            className="ms-3 text-danger fs-4 bg-transparent border-0"
+            onClick={() => showModal(proCatState[i]._id)}
+          >
             <AiOutlineDelete />
-          </Link>
+          </button>
         </>
       ),
     });
   }
+
+  const deleteprodcat = (e) => {
+    dispatch(deleteProductCat(e));
+    setOpen(false);
+    setTimeout(() => {
+      dispatch(getProductCat());
+    }, 100);
+  };
   return (
     <div>
       <h3 className="mb-4 title">Product Categories</h3>
       <div>
         <Table columns={columns} dataSource={data1} />
       </div>
+      <CustomModal
+        hideModal={hideModal}
+        open={open}
+        performAction={() => deleteprodcat(prodcatId)}
+        title="Ary you Sure you want to delete this Product Category ?"
+      />
     </div>
   );
 };
