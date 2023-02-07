2a10b1c191265ebcbfcae71e4c4fe6b8598b085f
Yash <yashrajoriya321@gmail.com>
2023-02-07 23:36:13 +0530

    update

--- src/features/pcategory/pCategoryService.js
+++ src/features/pcategory/pCategoryService.js
@@ -10,8 +10,27 @@ const createProCat = async (prodcat) => {
   const response = await axios.post(`${base_url}category/`, prodcat, config);
   return response.data;
 };
+const updateProCat = async (category) => {
+  const response = await axios.put(
+    `${base_url}category/${category.id}`,
+    { title: category.procatData.title },
+    config
+  );
+  return response.data;
+};
+const getAProCat = async (id) => {
+  const response = await axios.get(`${base_url}category/${id}`, config);
+  return response.data;
+};
+const deleteProCat = async (id) => {
+  const response = await axios.delete(`${base_url}category/${id}`, config);
+  return response.data;
+};
 const pCatService = {
   getProductCat,
   createProCat,
+  updateProCat,
+  getAProCat,
+  deleteProCat,
 };
 export default pCatService;
--- src/features/pcategory/pCategorySlice.js
+++ src/features/pcategory/pCategorySlice.js
@@ -11,12 +11,41 @@ export const getProductCat = createAsyncThunk(
     }
   }
 );
-
+export const getAProductCat = createAsyncThunk(
+  "category/get-procategory",
+  async (id, thunkAPI) => {
+    try {
+      return await pCatService.getAProCat(id);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
 export const createProCat = createAsyncThunk(
   "productCategories/create-categories",
   async (procatData, thunkAPI) => {
     try {
       return await pCatService.createProCat(procatData);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+export const updateProductCat = createAsyncThunk(
+  "category/update-category",
+  async (category, thunkAPI) => {
+    try {
+      return await pCatService.updateProCat(category);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+export const deleteProductCat = createAsyncThunk(
+  "category/delete-category",
+  async (id, thunkAPI) => {
+    try {
+      return await pCatService.deleteProCat(id);
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
     }
@@ -59,9 +88,54 @@ export const proCategories = createSlice({
         state.isLoading = false;
         state.isError = false;
         state.isSuccess = true;
-        state.CreatedproCategories = action.payload;
+        state.createdproCategories = action.payload;
       })
       .addCase(createProCat.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+      })
+      .addCase(getAProductCat.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getAProductCat.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.categoryName = action.payload.title;
+      })
+      .addCase(getAProductCat.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+      })
+      .addCase(updateProductCat.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(updateProductCat.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.updatedproCategories = action.payload;
+      })
+      .addCase(updateProductCat.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+      })
+      .addCase(deleteProductCat.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(deleteProductCat.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.deletedproCategories = action.payload;
+      })
+      .addCase(deleteProductCat.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = true;
         state.isSuccess = false;
