6a4c6a929393a5b47b52f80f5075cd8f59bdf983
Yash <yashrajoriya321@gmail.com>
2023-02-09 00:28:07 +0530

    update

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import customerReducer from "../features/customer/customerSlice";
+import enquiryReducer from "../features/enquiry/enquirySlice";
 // import productReducer from "../features/product/productSlice";
 // import brandReducer from "../features/brand/brandSlice";
 // import proCategoriesReducer from "../features/pcategory/pCategorySlice";
 // import colorReducer from "../features/color/colorSlice";
-// import enquiryReducer from "../features/enquiry/enquirySlice";
 // import blogReducer from "../features/blog/blogSlice";
 // import blogCategoriesReducer from "../features/bCategory/blogCatSlice";
 // import orderReducer from "../features/auth/authSlice";
@@ -16,13 +16,13 @@ export const store = configureStore({
   reducer: {
     auth: authReducer,
     customer: customerReducer,
+    enquiry: enquiryReducer,
     // product: productReducer,
     // brand: brandReducer,
     // pCategory: proCategoriesReducer,
     // bCategory: blogCategoriesReducer,
     // color: colorReducer,
     // blog: blogReducer,
-    // enquiry: enquiryReducer,
     // order: orderReducer,
     // upload: uploadReducer,
     // coupon: couponReducer,
