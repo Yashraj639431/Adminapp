da5e8d73f4d048ff64834fc21da829108bb56592
Yash <yashrajoriya321@gmail.com>
2023-02-09 18:02:05 +0530

    update

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import customerReducer from "../features/customer/customerSlice";
 import productReducer from "../features/product/productSlice";
+import brandReducer from "../features/brand/brandSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     product: productReducer,
     customer: customerReducer,
+    brand: brandReducer,
   },
 });
--- src/pages/BrandList.js
+++ src/pages/BrandList.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { Table } from "antd";
+import { useDispatch, useSelector } from "react-redux";
+import { getBrands } from "../features/brand/brandSlice";
 
-const BrandLIst = () => {
+const columns = [
+  {
+    title: "SNo.",
+    dataIndex: "key",
+  },
+  {
+    title: "Name",
+    dataIndex: "name",
+    defaultSortOrder: "descend",
+    sorter: (a, b) => a.name.length - b.name.length,
+  },
+  {
+    title: "Action",
+    dataIndex: "action",
+  },
+];
+
+const Brands = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBrands());
+  }, [dispatch]);
+
+  const brandState = useSelector((state) => state.brand.brands);
+  const data1 = [];
+  
+  for (let i = 0; i < brandState.length; i++) {
+      data1.push({
+        key: i + 1,
+        name: brandState[i].title,
+      });
+  }
+  
   return (
     <div>
-      BrandList
+      <h3 className="mb-4 title">Brands</h3>
+      <div>
+        <Table columns={columns} dataSource={data1} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BrandLIst
+export default Brands;
