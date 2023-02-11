c696d207061e059c6b7ff42ad4de80c64c730983
Yash <yashrajoriya321@gmail.com>
2023-02-11 21:54:57 +0530

    up

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -1,26 +1,19 @@
 import React from "react";
-import CustomInput from "../components/CustomInput";
+import { CiMail } from "react-icons/ci";
 
 const ForgotPassword = () => {
   return (
-    <div className="py-5" style={{ background: "#ffd333", minHeight: "100vh" }}>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <div className="my-5 w-25 bg-white rounded-3 mx-auto p-4">
-        <h3 className="text-center title">Forgot Password</h3>
-        <p className="text-center">Please enter your register email to get reset password mail.</p>
+    <div className="section">
+      <div className="form-box w-25">
         <form action="">
-          <CustomInput type="password" label="Email Address" id="email" />
-          <button
-            className="border-0 px-3 py-2 text-white fw-bold w-100"
-            style={{ background: "#ffd333" }}
-            type="submit"
-          >
-            Send Link
-          </button>
+          <h3>Forgot Password</h3>
+          <p>Please enter your register email to get reset password mail.</p>
+          <div className="inputbox w-100">
+            <CiMail className="react-icons" />
+            <input type="password" id="email" required/>
+            <label>Email Address</label>
+          </div>
+          <button type="submit">Send Link</button>
         </form>
       </div>
     </div>
